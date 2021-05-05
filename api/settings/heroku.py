ad3dc8c6b4072d9ba13ef8acbb878da2b1d693ae
Stengrat <ygorsa1998@hotmail.com>
2021-05-05 18:58:13 -0300

    teste

--- api/settings/heroku.py
+++ api/settings/heroku.py
@@ -11,5 +11,12 @@ SECRET_KEY = env("SECRET_KEY")
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 DATABASES = {
-    "default": env.db(),
+    "default": {        
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'listafacil',
+        'USER': 'listafacil',
+        'PASSWORD': 'p73W54ARuE8oM6oi',
+        'HOST': '144.91.110.253',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+        }
 }
